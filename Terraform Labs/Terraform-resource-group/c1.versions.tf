c07565e3c2c2b87adebf4eaff27c8b65ddc548ac
unknown <a30sham.07@gmail.com>
2022-06-22 17:39:42 +0000

    Terraform RG lab

---
+++ Terraform Labs/Terraform-resource-group/c1.versions.tf
@@ -0,0 +1,14 @@
+#specifying the provider and version
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
---
+++ Terraform Labs/Terraform-resource-group/variables.tf
@@ -0,0 +1,9 @@
+variable "azurerm_resource_group_prefix" {
+  default     = "myresource-group"
+  description = "Creating resource group name"
+}
+
+variable "azurerm_resource_group_location" {
+  default     = "eastus"
+  description = "Providing default resource group"
+}
